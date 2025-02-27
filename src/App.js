a4e942f60c64a498603ae97d14c17387b5e5593e
Bouria <ibrahimabouriadiallo@gmail.com>
2025-02-27 23:09:09 +0100

    🎉App.js > Add my firstname

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Bouria
+          Hello Bouria Kitofy
         </p>
         <a
           className="App-link"
