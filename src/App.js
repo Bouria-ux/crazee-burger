4b6e21d8f4837e178b3b819d378f7c54b24c5fa4
Bouria <ibrahimabouriadiallo@gmail.com>
2025-02-28 02:20:23 +0100

    ♻️ F01 : LOGIN> Create LoginForm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import LoginPage from './components/Pages/LoginPage';
+import LoginPage from './components/Pages/login/LoginPage';
+
 
 function App() {
   return <LoginPage/>
