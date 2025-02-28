72ce464a84c4f2275ba079b125206ae9f2848151
Bouria <ibrahimabouriadiallo@gmail.com>
2025-02-28 01:58:05 +0100

    ✨ F01: Add title and form

--- src/components/Pages/LoginPage.js
+++ src/components/Pages/LoginPage.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function LoginPage() {
+    // state
+    const [inputValue, setinputValue] = useState("")
+
+    // comportements
+
+    const handleSubmit = (event) => { 
+        event.preventDefault()
+        alert(`Bonjour ${inputValue}`)
+        setinputValue("")
+     }
+
+ const handleChange = (event) => { 
+  setinputValue(event.target.value)
+
+  }
+     
+    // affichage (render)
+
   return (
-    <div>LoginPage</div>
+    <div>
+        <h1>Bienvenue chez nous</h1>
+        <br/>
+        <h2>Connectez-vous</h2>
+        <form action="submit" onSubmit={handleSubmit}>
+            <input value={inputValue} onChange={handleChange} type="text" placeholder="Entrez votre prenom..." required />
+            <button>Accédez a votre espace</button>
+
+        </form>
+    </div>
   )
 }
