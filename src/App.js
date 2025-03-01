f08930558862d429f04c1dc6edfb601cf98c2a76
Bouria <ibrahimabouriadiallo@gmail.com>
2025-03-01 03:55:02 +0100

    ✨ F02: APP > install routing system

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './components/Pages/login/LoginPage';
+import { Route, Routes } from 'react-router-dom';
+import Orderpage from './components/Pages/Order/Orderpage';
+import ErreurPage from './components/Pages/erreur/ErreurPage';
+
+
+
 
 
 function App() {
-  return <LoginPage/>
+  return (
+  <Routes>
+    <Route path="/" element={<LoginPage />}/>
+    <Route path="/order" element={<Orderpage />}/>
+    <Route path="*" element={<ErreurPage />}/>
+    
+  </Routes>
+  )
 }
 
 export default App;
