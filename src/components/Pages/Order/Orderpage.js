7de94a4d8de37a960b2d74d06ad428487cc82c40
Bouria <ibrahimabouriadiallo@gmail.com>
2025-03-05 05:49:39 +0100

    🚧 F04 : ORDER > layout > add container

--- src/components/Pages/Order/Orderpage.js
+++ src/components/Pages/Order/Orderpage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, useParams } from "react-router-dom"
+import styled from 'styled-components'
 
 export default function Orderpage() {
   //State
@@ -8,9 +9,26 @@ export default function Orderpage() {
   console.log("username:", username)
   //Affichage
   return (
-    <div>
-      <h1>Bonjour {username}</h1>
-        <Link to="/"> <button>Deconexion</button></Link>
+    <OrderpageStyled>
+      <div className="container">
+        <h1>Bonjour {username}</h1>
+          <Link to="/">
+          <button>Deconexion</button></Link>
       </div>
+      </OrderpageStyled>
   )
 }
+
+const OrderpageStyled = styled.div`
+background: orange;
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+
+.container{
+  background: red;
+  height: 95vh;
+  width: 1400px;
+}
+`;
